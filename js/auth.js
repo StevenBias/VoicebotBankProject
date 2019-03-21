ebbcabbf1c1122c112d1f5c2a1478c1507e4db88
hanadyabboud <44815451+hanadyabboud@users.noreply.github.com>
2019-03-21 13:34:57 +0100

    Add log to test authentification

--- js/auth.js
+++ js/auth.js
@@ -3,11 +3,13 @@ var gapi = gapi || {};
 // [START load_auth2_library]
 function loadAuthClient(){
    gapi.load('client:auth2', initGoogleAuth);
+   console.log("load");
 }
 // [END load_auth2_library]
 
 // [START init_google_auth]
 function initGoogleAuth(clientId = '225209884470-vrbburpnhs875a10dukuf1v6sb4quqj6.apps.googleusercontent.com'){
+   console.log("init");
    var SCOPE = 'https://www.googleapis.com/auth/cloud-platform https://www.googleapis.com/auth/dialogflow';
    gapi.auth2.init({
       clientId: clientId,
@@ -20,7 +22,7 @@ function initGoogleAuth(clientId = '225209884470-vrbburpnhs875a10dukuf1v6sb4quqj
 function getToken() {
    var user = gapi.auth2.getAuthInstance().currentUser.get();
    var token = user.getAuthResponse().access_token;
+   console.log(token);
    return token;
 }
 // [END user_signin]
-
