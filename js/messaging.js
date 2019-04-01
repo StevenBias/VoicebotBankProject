f02af1bcad522428e4426fdde18145c4e8f0e16e
Steven Bias <bias_steven@hotmail.fr>
2019-04-01 13:34:25 +0200

    WIP

--- js/messaging.js
+++ js/messaging.js
@@ -15,3 +15,21 @@ function addError(text) {
    const footer = document.querySelector(".app-footer");
    footer.style.display = "none";
 }
+
+function loadChatbox(){
+   var e=document.getElementById("app-header");
+   e.style.display="block";
+   var e=document.getElementById("app-content");
+   e.style.display="block";
+   var e=document.getElementById("app-footer");
+   e.style.display="block";
+}
+
+function reduceChatbox(){
+   var e=document.getElementById("app-header");
+   e.style.display="block";
+   var e=document.getElementById("app-content");
+   e.style.display="none";
+   var e=document.getElementById("app-footer");
+   e.style.display="none";
+}
