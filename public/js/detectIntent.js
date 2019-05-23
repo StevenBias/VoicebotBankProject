7b10ce71f80b83c1bf95ba031912452320b69a81
Steven Bias <bias_steven@hotmail.fr>
2019-05-23 18:10:00 +0200

    add missing folders

---
+++ public/js/detectIntent.js
@@ -0,0 +1,73 @@
+/**
+ * Copyright 2018, Google, LLC.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+
+//const projectId = process.env.GCLOUD_PROJECT;
+//const util = require('util');
+
+async function detectIntent() {
+  // [START dialogflow_detect_intent_with_texttospeech_response]
+  // Imports the Dialogflow client library
+  const dialogflow = require('dialogflow').v2beta1;
+
+  // Instantiate a DialogFlow client.
+  const sessionClient = new dialogflow.SessionsClient();
+
+  const projectId = 'guide-cetelem';
+  const sessionId = '1234';
+  const query = 'bonjour';
+  const languageCode = 'fr-FR';
+//  const outputFile = `path for audio output file, e.g. ./resources/myOutput.wav`;
+
+  // Define session path
+  const sessionPath = sessionClient.sessionPath(projectId, sessionId);
+  const fs = require(`fs`);
+
+  // The audio query request
+  const request = {
+    session: sessionPath,
+    queryInput: {
+      text: {
+        text: query,
+        languageCode: languageCode,
+      },
+    },
+    outputAudioConfig: {
+      audioEncoding: `OUTPUT_AUDIO_ENCODING_LINEAR_16`,
+    },
+  };
+
+  // Send request and log result
+  const responses = await sessionClient.detectIntent(request);
+  console.log('Detected intent');
+
+  const result = responses[0].queryResult;
+  console.log(`  Query: ${result.queryText}`);
+  console.log(`  Response: ${result.fulfillmentText}`);
+  if (result.intent) {
+    console.log(`  Intent: ${result.intent.displayName}`);
+  } else {
+    console.log(`  No intent matched.`);
+  }
+
+  const audioFile = responses[0].outputAudio;
+//  await util.promisify(fs.writeFile)(outputFile, audioFile, 'binary');
+//  console.log(`Audio content written to file: ${outputFile}`);
+  // [END dialogflow_detect_intent_with_texttospeech_response]
+}
+
+
+detectIntent();
